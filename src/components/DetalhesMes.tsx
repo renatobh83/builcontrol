5bf5dbcefc72da774192638cffc3e655939b92f0
renatobh83 <neo_rlm@hotmail.com>
2021-09-05 13:59:17 -0300

    changeDeleteAction

--- src/components/DetalhesMes.tsx
+++ src/components/DetalhesMes.tsx
@@ -11,31 +11,9 @@ import {
   Grid,
 } from "semantic-ui-react";
 import { useAppContext } from "../context/AppContext";
+import GridCompras from "./GridCompras";
 import { groupby } from "../utils/filterDates";
-function confirmDelete(state, action) {
-  switch (action.type) {
-    case "ABERTURA":
-      return {
-        openConfirm: true,
-      };
-    case "CONFIRMA":
-      return {
-        log: {
-          type: "Confirma",
-          id: action.id,
-        },
-        ...state.log,
 
-        openConfirm: true,
-      };
-    case "CANCELA":
-      return {
-        openConfirm: false,
-      };
-    default:
-      throw new Error();
-  }
-}
 export default function DetalhesMes() {
   const {
     comprasMes,
@@ -52,41 +30,15 @@ export default function DetalhesMes() {
   const [open, setOpen] = useState(false);
   const [open2, setOpen2] = useState(false);
 
-  const [state, dispatch] = useReducer(confirmDelete, {
-    log: null,
-    openConfirm: false,
-  });
+  // const handleEditar = (compra) => {
+  //   if (compra.formaPagamento !== "credito") {
+  //     setEditarCompra(compra);
+  //     toggleActive();
+  //   } else {
+  //     setOpen(true);
+  //   }
+  // };
 
-  const { log, openConfirm } = state;
-
-  const handleEditar = (compra) => {
-    if (compra.formaPagamento !== "credito") {
-      setEditarCompra(compra);
-      toggleActive();
-    } else {
-      setOpen(true);
-    }
-  };
-
-  const apagar = useCallback(async () => {
-    if (log) {
-      const id = log.id;
-      await fetch(`/api/compras/loadInsert?identifier=${id.identifier}`, {
-        method: "DELETE",
-      });
-      const newArray = compratoFetch.filter(
-        (idCompra) => idCompra.identifier !== id.identifier
-      );
-
-      setComprastoFetch(newArray);
-      dataFetch(newArray);
-      setDetalhes(false);
-    }
-  }, [log]);
-
-  (async () => {
-    apagar();
-  })();
   let totalCasa = 0;
   let totalPessoal = 0;
   useEffect(() => {
@@ -117,194 +69,10 @@ export default function DetalhesMes() {
             <Segment raised key={compra._id}>
               <Grid columns="equal">
                 <Grid.Row only="mobile">
-                  <Grid.Column>
-                    <Item>
-                      <Item.Content>
-                        <Item.Header>{compra.descr}</Item.Header>
-                        <Item.Meta>
-                          <span className="price">R$ {compra.valor} </span>
-
-                          <Grid.Row>
-                            {compra.numParcela && (
-                              <span className="stay">
-                                Parcelado {compra.numParcela} -{" "}
-                                {compra.parcelas}
-                              </span>
-                            )}
-                            {compra.recorrente && (
-                              <span className="stay">Recorrente</span>
-                            )}
-                          </Grid.Row>
-                          {compra.numParcela && (
-                            <Grid.Row>
-                              {compra.parcelas - compra.numParcela === 0
-                                ? ""
-                                : `Resta : ${(
-                                    compra.valor *
-                                    (compra.parcelas - compra.numParcela)
-                                  ).toFixed(2)}`}
-                            </Grid.Row>
-                          )}
-                        </Item.Meta>
-                      </Item.Content>
-                    </Item>
-                  </Grid.Column>
-                  <Grid.Column textAlign="center">
-                    <Grid.Row>{compra.categoria}</Grid.Row>
-                    <Divider />
-                    <Button.Group size="mini">
-                      {/* <Button
-                        positive
-                        onClick={() => handleEditar(compra)}
-                        disabled
-                      >
-                        Editar
-                      </Button>
-                      <Button.Or text="Ou" /> */}
-                      <Modal
-                        onOpen={(e) =>
-                          dispatch({
-                            type: "ABERTURA",
-                          })
-                        }
-                        onClose={(e) =>
-                          dispatch({
-                            type: "CANCELA",
-                          })
-                        }
-                        open={openConfirm}
-                        trigger={<Button negative>Apagar </Button>}
-                      >
-                        <Modal.Header>Apagar movimento</Modal.Header>
-                        <Modal.Content>
-                          <p>Voce esta certo que deseja apagar movimento</p>
-                        </Modal.Content>
-                        <Modal.Actions>
-                          <Button
-                            onClick={(e) =>
-                              dispatch({
-                                type: "CANCELA",
-                              })
-                            }
-                            negative
-                          >
-                            Não
-                          </Button>
-                          <Button
-                            onClick={(e) =>
-                              dispatch({
-                                id: compra,
-                                type: "CONFIRMA",
-                              })
-                            }
-                            positive
-                          >
-                            Sim
-                          </Button>
-                        </Modal.Actions>
-                      </Modal>
-                    </Button.Group>
-                  </Grid.Column>
+                  <GridCompras compra={compra} />
                 </Grid.Row>
                 <Grid.Row only="computer tablet">
-                  <Grid.Column>
-                    <Item.Group>
-                      <Item>
-                        <Item.Content>
-                          <Item.Header>{compra.descr}</Item.Header>
-                          <Item.Meta>
-                            <span className="price">R$ {compra.valor} </span>
-                          </Item.Meta>
-                          <Item.Description>
-                            {compra.categoria}
-                          </Item.Description>
-                        </Item.Content>
-                      </Item>
-                    </Item.Group>
-                  </Grid.Column>
-                  <Grid.Column>
-                    <Item.Description>
-                      {compra.numParcela && (
-                        <>
-                          <span className="stay">
-                            Parcelado {compra.numParcela} -{compra.parcelas}{" "}
-                          </span>
-                          <Grid.Row>
-                            {compra.parcelas - compra.numParcela === 0
-                              ? ""
-                              : `Resta : ${(
-                                  compra.valor *
-                                  (compra.parcelas - compra.numParcela)
-                                ).toFixed(2)}`}
-                          </Grid.Row>
-                        </>
-                      )}
-                      {compra.recorrente && (
-                        <span className="stay">Recorrente</span>
-                      )}
-                    </Item.Description>
-                  </Grid.Column>
-                  <Grid.Column
-                    width="5"
-                    textAlign="center"
-                    verticalAlign="middle"
-                  >
-                    <Button.Group>
-                      {/* <Button
-                        positive
-                        onClick={() => handleEditar(compra)}
-                        disabled
-                      >
-                        Editar
-                      </Button>
-                      <Button.Or text="Ou" /> */}
-                      <Modal
-                        onOpen={(e) =>
-                          dispatch({
-                            type: "ABERTURA",
-                          })
-                        }
-                        onClose={(e) =>
-                          dispatch({
-                            type: "CANCELA",
-                          })
-                        }
-                        open={openConfirm}
-                        trigger={<Button negative>Apagar </Button>}
-                      >
-                        <Modal.Header>Apagar movimento</Modal.Header>
-                        <Modal.Content>
-                          <p>Voce esta certo que deseja apagar movimento</p>
-                        </Modal.Content>
-                        <Modal.Actions>
-                          <Button
-                            onClick={(e) =>
-                              dispatch({
-                                type: "CANCELA",
-                              })
-                            }
-                            negative
-                          >
-                            Não
-                          </Button>
-                          <Button
-                            onClick={(e) =>
-                              dispatch({
-                                id: compra,
-                                type: "CONFIRMA",
-                              })
-                            }
-                            positive
-                          >
-                            Sim
-                          </Button>
-                        </Modal.Actions>
-                      </Modal>
-                      {/* <Button negative onClick={() => handleApagar(compra)}>
-                        Apagar{" "}
-                      </Button> */}
-                    </Button.Group>
-                  </Grid.Column>
+                  <GridCompras compra={compra} />
                 </Grid.Row>
               </Grid>
             </Segment>
---
+++ src/components/GridCompras.tsx
@@ -0,0 +1,83 @@
+import { useCallback, useState } from "react";
+import { Button, Item, Grid, Modal, Header, Icon } from "semantic-ui-react";
+import { useAppContext } from "../context/AppContext";
+export default function GridCompras({ compra }) {
+  const [open, setOpen] = useState(false);
+  const { compratoFetch, setDetalhes, setComprastoFetch, dataFetch } =
+    useAppContext();
+
+  const apagar = useCallback(async (id) => {
+    await fetch(`/api/compras/loadInsert?identifier=${id.identifier}`, {
+      method: "DELETE",
+    });
+    const newArray = compratoFetch.filter(
+      (idCompra) => idCompra.identifier !== id.identifier
+    );
+    setComprastoFetch(newArray);
+    dataFetch(newArray);
+    setDetalhes(false);
+  }, []);
+  return (
+    <>
+      <Grid.Column>
+        <Item.Group>
+          <Item>
+            <Item.Content>
+              <Item.Header>{compra.descr}</Item.Header>
+              <Item.Meta>
+                <span className="price">R$ {compra.valor} </span>
+              </Item.Meta>
+              <Item.Description>{compra.categoria}</Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Grid.Column>
+      <Grid.Column>
+        <Item.Description>
+          {compra.numParcela && (
+            <>
+              <span className="stay">
+                Parcelado {compra.numParcela} -{compra.parcelas}{" "}
+              </span>
+              <Grid.Row>
+                {compra.parcelas - compra.numParcela === 0
+                  ? ""
+                  : `Resta : ${(
+                      compra.valor *
+                      (compra.parcelas - compra.numParcela)
+                    ).toFixed(2)}`}
+              </Grid.Row>
+            </>
+          )}
+          {compra.recorrente && <span className="stay">Recorrente</span>}
+        </Item.Description>
+      </Grid.Column>
+      <Grid.Column width="5" textAlign="center" verticalAlign="middle">
+        <Button negative onClick={() => setOpen(true)}>
+          Apagar
+        </Button>
+      </Grid.Column>
+      <Modal
+        closeIcon
+        open={open}
+        dimmer="blurring"
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+      >
+        <Header content="Apagar movimento" />
+        <Modal.Content>
+          <p>Voce esta certo que deseja apagar movimento </p>
+          <p>Esta operacao nao podera ser desfeita</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="red" onClick={() => setOpen(false)}>
+            <Icon name="remove" /> Nao
+          </Button>
+          <Button color="green" onClick={() => apagar(compra)}>
+            <Icon name="checkmark" /> Sim
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    </>
+  );
+}
